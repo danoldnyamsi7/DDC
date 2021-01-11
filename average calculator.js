eae3b1b9bf2f92db508836b002c3bdc02fac31b4
danoldnyamsi7 <73864366+danoldnyamsi7@users.noreply.github.com>
2021-01-11 01:20:28 +0000

    Update average calculator.js

--- average calculator.js
+++ average calculator.js
@@ -1,7 +1,5 @@
 
 //when using rest and spread operators it is of no use to use an array and push values to it the rest operator does that for you
 
-let average = (...numbers) => {
-    let getAverage = numbers.reduce((sum,number) =>{sum + number},0)/numbers.length;
-}
+let average = (...numbers) => numbers.reduce((sum,number) => sum + number,0)/numbers.length;
 average(1,2,3,4,5);
